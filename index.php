35d06f1a809b0fc019834f4bdaa11d724c21cb5e
Ifex <if3cofr3sh@gmail.com>
2020-10-02 11:38:23 +0100

    setup sass

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Sirimazone Site</title>
-	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="styles/css/style.css">
 </head>
 <body>
 
 	<h1>Sirimazone</h1>
+	<p>beginning the sirimazone project</p>
 
 	
+	<script src="scripts/js/script.js" defer></script>
 </body>
 </html>
