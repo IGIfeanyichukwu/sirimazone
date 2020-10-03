ac3d0e2b7ec08c32980e05f1db040551f8721ca2
Ifex <if3cofr3sh@gmail.com>
2020-10-03 14:06:09 +0100

    added font, icon and separated sass files

--- index.php
+++ index.php
@@ -7,10 +7,52 @@
 	<link rel="stylesheet" href="styles/css/style.css">
 </head>
 <body>
-
-	<h1>Sirimazone</h1>
-	<p>beginning the sirimazone project</p>
-
+<div class="container">
+    <header class="header">
+	<h2>Sirima<span>zone</span></h2>
+	<form action="">
+	    <input type="search" name="search" id="main-search" placeholder="Search here" class="header-search">
+	    <button><i class="fas fa-search"></i></button>
+	</form>
+	
+	<nav class="nav">
+        <ul>
+            <li><a href="#"><i class="fas fa-home"></i> Home</a></li>
+            <li><a href="#"><i class="fas fa-comment-alt"></i> Feedback</a></li>
+            <li><a href="#"><i class="fas fa-fire"></i><span></span>Hollywood</a></li>
+            <li><a href="#"><i class="fas fa-fire"></i><span></span>Nollywood</a></li>
+            <li><a href="#"><i class="fas fa-fire"></i><span></span>Bollywood</a></li>
+            <li><a href="#"><i class="fas fa-fire"></i><span></span>Others</a></li>
+        </ul>
+	</nav>
+	
+	<div class="random-movie-container">
+	    <h3>Random Movies</h3>
+	    <div class="random-movie-wrap">
+	        <ul>
+	            <li><a href="#">Movie One</a></li>
+	            <li><a href="#">Movie Two</a></li>
+	            <li><a href="#">Movie Three</a></li>
+	            <li><a href="#">Movie Four</a></li>
+	        </ul>
+	    </div>
+	    
+	    
+	</div>
+	
+	<div class="login-icon-wrap">
+       <div><span><i class="fas fa-user"></i></span></div> 
+       <div><span>Guest</span></div>
+	</div>
+	
+	</header>
+	<div class="main">
+            <div class="main-display">
+                This is the main display
+                all stuff go in here
+            </div>  
+	</div>
+</div>
 	
 	<script src="scripts/js/script.js" defer></script>
 </body>
